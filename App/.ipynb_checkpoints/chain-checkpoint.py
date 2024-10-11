03987b3cc595d76cc2fb784ca784b68c7873c55f
Kshitij Angurala <136297173+KshitijAng@users.noreply.github.com>
2024-10-11 19:31:59 +0530

    First Commit

--- App/.ipynb_checkpoints/chain-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import os
-from langchain_groq import ChatGroq
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.exceptions import OutputParserException
-from dotenv import load_dotenv
-
-load_dotenv()
--- App/.ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("📧 Cold Mail Generator")
-url_input = st.text_input("Enter a URL:", value="https://jobs.nike.com/job/R-33460")
-submit_button = st.button("Submit")
-
-if submit_button:
-    st.code("Hello Hiring Manager")
